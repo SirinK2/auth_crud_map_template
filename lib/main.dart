0f24f2b8f55d0fd65529b94c2f919106a14e88b1
Khairiah Mohammed <khairiahalayyafi@gmail.com>
2023-03-05 09:30:29 +0300

    feat: add theme app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import 'package:auth_crud_map_template/core/routes/route.dart';
+=======
+import 'package:auth_crud_map_template/core/theme/theme.dart';
+import 'package:auth_crud_map_template/features/auth/auth.dart';
+>>>>>>> f6b18b3 (feat: add theme app)
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -19,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+<<<<<<< HEAD
       locale: controller.initialLanguage,
       translations: TranslationsMap(),
       theme: ThemeData(
@@ -26,6 +32,11 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: Routes.translation,
       getPages: AppRoutes.routes,
+=======
+      theme: ThemeApp.lightTheme,
+      darkTheme:ThemeApp.darkTheme ,
+      home: Auth()
+>>>>>>> f6b18b3 (feat: add theme app)
     );
   }
 }
