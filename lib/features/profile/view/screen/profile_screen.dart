0928402a67e1c4d4b7a35681594d1b80d3019384
Khairiah Mohammed <khairiahalayyafi@gmail.com>
2023-03-09 11:45:18 +0300

    feat: add all the profile feat

--- lib/features/profile/view/screen/profile_screen.dart
+++ lib/features/profile/view/screen/profile_screen.dart
@@ -3,7 +3,7 @@ import 'package:auth_crud_map_template/core/routes/route.dart';
 import 'package:auth_crud_map_template/features/profile/controller/profile_conroller.dart';
 import 'package:auth_crud_map_template/features/profile/view/widget/darkmode_widget.dart';
 import 'package:auth_crud_map_template/features/profile/view/widget/logout_widget.dart';
-import 'package:auth_crud_map_template/features/profile/view/widget/notafaction_widget.dart';
+import 'package:auth_crud_map_template/features/profile/view/widget/localization_widget.dart';
 import 'package:auth_crud_map_template/features/profile/view/widget/profile_header_widget.dart';
 import 'package:auth_crud_map_template/features/profile/view/widget/profile_widget.dart';
 import 'package:flutter/material.dart';
